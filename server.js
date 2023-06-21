522f29ae6015fb6a2181f58abf3d2f8aec6d41a4
Lalit9025 <99483006+Lalit9025@users.noreply.github.com>
2023-06-21 10:48:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl')
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL,{
+mongoose.connect('process.env.MONGO_URL',{
        useNewUrlParser: true, 
        useUnifiedTopology : true,
 })
